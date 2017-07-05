9d56eecc200a80f1f73ef05ea012d64513913437
AMR-PC\amrbadr12 <amr.badr.9620@gmail.com>
2017-07-05 17:39:58 +0200

    Setting up the Unreal project

---
+++ TankCombat/Source/TankCombat/TankCombatGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TankCombatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKCOMBAT_API ATankCombatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
